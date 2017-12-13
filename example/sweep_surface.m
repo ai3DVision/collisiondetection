33236265c457cd6cd77a19e7938192d2fb92abd9
Alec Jacobson <alecjacobson@gmail.com>
2017-12-13 09:33:40 -0500

    attempt at using ctcd for swept surfaces

--- example/sweep_surface.m
+++ example/sweep_surface.m
@@ -4,18 +4,28 @@ function [SV,SF,G,hitG,side] = sweep_surface(VV,F)
     in = all(bsxfun(@ge,G,min(V)-eta),2) & all(bsxfun(@le,G,max(V)+eta),2);
   end
 
-  sx = 100;
+  sx = 40;
   fprintf('building grid...\n');
   [G,side,r] = voxel_grid(reshape(permute(VV,[1 3 2]),[],3),sx,'Pad',2);
   eta = min(r);
   hitG = false(size(G,1),1);
-  fprintf('dtcd...\n');
 
-  for t = 1:size(VV,3)
-    not_hit = find(~hitG);
-    not_hit = not_hit(in_bb(G(not_hit,:),VV(:,:,t),eta));
-    fprintf('  %d\n',numel(not_hit));
-    hitG(not_hit) = signed_distance(G(not_hit,:),VV(:,:,t),F,'SignedDistanceType','pseudonormal')<eta;
+  fprintf('dtcd...\n');
+  for t = 1:size(VV,3)-1
+    nss = 0;
+    substeps = linspace(0,1,2+nss);
+    if t ~= 1
+      substeps = substeps(2:end);
+    end
+    % linear sub-stepping
+    for s = substeps
+      not_hit = find(~hitG);
+      VVs = VV(:,:,t) + s*(VV(:,:,t+1) - VV(:,:,t));
+      not_hit = not_hit(in_bb(G(not_hit,:),  VVs,eta));
+      new_hit = signed_distance(G(not_hit,:),VVs,F,'SignedDistanceType','pseudonormal')<eta;
+      fprintf('  %d -> %d\n',numel(not_hit),sum(new_hit));
+      hitG(not_hit) = new_hit;
+    end
   end
 
   %clf;
@@ -31,18 +41,36 @@ function [SV,SF,G,hitG,side] = sweep_surface(VV,F)
   for t = 2:size(VV,3)
     not_hit = find(~hitG);
     not_hit = not_hit(in_bb(G(not_hit,:),[VV(:,:,t);VV(:,:,t-1)],eta));
-    fprintf('  %d\n',size(not_hit,1));
     V0 = [VV(:,:,t-1);G(not_hit,:)];
     V1 = [VV(:,:,t  );G(not_hit,:)];
     % There can't be edge-edge collisions
-    VF = ctcd(V0,F,V1,'OuterEta',eta,'InnerEta',eta);
-    hitG(not_hit(VF(VF(:,1)>size(VV,1),1)-size(VV,1))) = true;
+    C = [ones(size(VV,1),1);2*ones(size(not_hit,1),1)];
+    VF = ctcd(V0,F,V1,'OuterEta',eta,'InnerEta',eta,'Components',C);
+    new_hit = VF(VF(:,1)>size(VV,1),1)-size(VV,1);
+    hitG(not_hit(new_hit)) = true;
+    fprintf('  %d -> %d\n',size(not_hit,1),size(new_hit,1));
     %s.CData = 1*hitG;
     %tsh.Vertices = V1;
     %drawnow;
   end
   resh = @(X) reshape(X,side([2 1 3]));
+
+  fprintf('filter...\n');
+  hitG = resh(hitG);
+  dilate_side = [3 3 3];
+  hitG = imdilate(hitG,ones(dilate_side));
+  smooth_k = 3;
+  hitG = smooth3(hitG,'box',smooth_k);
+
   fprintf('isosurface...\n');
-  [SF,SV] = isosurface( resh(G(:,1)), resh(G(:,2)), resh(G(:,3)), resh(hitG));
+  level = 0.5;
+  [SF,SV] = isosurface(resh(G(:,1)), resh(G(:,2)), resh(G(:,3)),hitG,level);
+  % isosurface uses wrong orientation
+  SF = fliplr(SF);
+  decimate_factor = 0.1;
+  if decimate_factor < 1
+    fprintf('decimate...\n');
+    [SV,SF] = decimate_cgal(SV,SF,decimate_factor,'Adaptive',false);
+  end
 end
 
