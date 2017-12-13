d246ccb687a0fbaa41f935de2aaf77fb48628d9d
Alec Jacobson <alecjacobson@gmail.com>
2017-12-13 09:34:34 -0500

    rm private

--- src/CTCDNarrowPhase.h
+++ src/CTCDNarrowPhase.h
@@ -3,13 +3,16 @@
 
 #include "RetrospectiveDetection.h"
 
+// Alec: *NarrowPhase classes are really just namespaces (these functions are
+// effectively static and there are no members). record_min_t should just be a
+// parameter...
 class CTCDNarrowPhase : public NarrowPhase
 {
 public:
 	virtual void findCollisions(const History &h, const std::set<std::pair<VertexFaceStencil, double> > &candidateVFS, const std::set<std::pair<EdgeEdgeStencil, double> > &candidateEES,
 		std::set<VertexFaceStencil> &vfs, std::set<EdgeEdgeStencil> &ees);
 
-private:
+//private:
 	bool checkVFS(const History &h, VertexFaceStencil & vfs, double eta);
 	bool checkEES(const History &h, EdgeEdgeStencil & ees, double eta);
 };
